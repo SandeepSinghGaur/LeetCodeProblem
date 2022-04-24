0b4c974c52084aadb5d7237d072939e26bd8e5f3
Sandeep Singh <sandeepgaurdec13@gmail.com>
2022-04-24 23:30:47 +0530

    find Square root of a number

---
+++ Scaler/Advanced/Day-47-Binary-Search#02/sqrt.cpp
@@ -0,0 +1,43 @@
+/*
+Problem Description
+
+Given an integer A.
+
+Compute and return the square root of A.
+
+If A is not a perfect square, return floor(sqrt(A)).
+
+DO NOT USE SQRT FUNCTION FROM THE STANDARD LIBRARY.
+
+NOTE: Do not use the sqrt function from the standard library. Users are expected to solve this in O(log(A)) time.
+
+Problem Constraints
+0 <= A <= 10^10
+
+Input
+11
+Output
+3
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+ int sqrt(int A) {
+    int low=0,high=A-1;
+    int ans;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(mid*mid==A){
+            return mid;
+        }else if(mid*mid>A){
+            high=mid-1;
+        }else{
+            ans=mid;
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+int main(){
+   cout<<sqrt(11);
+}
