548c225e5f930e0cfb0470af725afcdc68980cee
Sandeep Singh <sandeepgaurdec13@gmail.com>
2022-05-04 16:00:37 +0530

    SubArray-With-Zero-Sum.

---
+++ Scaler/Advanced/Day-51-Hashing#01/SubArray-With-Zero-Sum.cpp
@@ -0,0 +1,17 @@
+/*
+Problem Description
+Given an array of integers A, find and return whether the given array contains a non-empty subarray with a sum equal to 0.
+
+If the given array contains a sub-array with sum zero return 1, else return 0.
+
+Problem Constraints
+1 <= |A| <= 100000
+
+-10^9 <= A[i] <= 10^9
+
+Input
+A = [1, 2, 3, 4, 5]
+Output
+0
+
+*/
