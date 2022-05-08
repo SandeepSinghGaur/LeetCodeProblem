9f786dd644671c68fdb2ebe3f3a98a9addbfcbce
Sandeep Singh <sandeepgaurdec13@gmail.com>
2022-05-08 19:41:00 +0530

    Largest Continuous Sequence Zero Sum

--- Scaler/Advanced/Day-51-Hashing#01/Largest-Continuous-Sequence-Zero-Sum.cpp
+++ Scaler/Advanced/Day-51-Hashing#01/Largest-Continuous-Sequence-Zero-Sum.cpp
@@ -15,3 +15,61 @@ A = [1,2,-2,4,-4]
 Output
 [2,-2,4,-4]
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> largest_Continuous_Sequences(vector<int>&A){
+        vector<long long>pf(A.size());
+      vector<int>result;
+      int n=A.size();
+     pf[0]=A[0];
+    for(int i=1;i<A.size();i++){
+        pf[i]=A[i]+pf[i-1];
+    }
+    int index=0;
+    for(int i=n-1;i>=0;i--){
+        if(pf[i]==0){
+         index=i;
+         break;
+        }
+         
+    }
+    int index2=INT_MIN;
+    int start,end;
+    bool check=true;
+    unordered_map<long long,int>um;
+   for(int i=0;i<A.size();i++){
+       if(um.find(pf[i])!=um.end()){
+           if(abs(i-um[pf[i]])>index2){
+               check=false;
+               index2=abs(i-um[pf[i]]);
+               start=um[pf[i]];
+               end=i;
+           }
+       }else{
+           um.insert({pf[i],i});
+       }
+   }
+   if(index==0 && check){
+       return result;
+   }
+   if(index+1>=end-start){
+       for(int i=0;i<=index;i++){
+           result.push_back(A[i]);
+       }
+   }else{
+       for(int i=start+1;i<=end;i++){
+           result.push_back(A[i]);
+       }
+   }
+  
+    return result;
+}
+int main(){
+    vector<int>v={1,2,-2,4,-4};
+    vector<int>result;
+    result=largest_Continuous_Sequences(v);
+    for(auto it:result){
+        cout<<it<<" ";
+    }
+}
