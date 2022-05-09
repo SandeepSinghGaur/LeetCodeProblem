f2c3fa22508e85298cef75a683c164596eb563e8
Sandeep Singh <sandeepgaurdec13@gmail.com>
2022-05-09 13:07:52 +0530

    DSA in JAVA started

---
+++ Scaler/Advanced/Day-51-Hashing#01/Distinct-Number-In-Window.cpp
@@ -0,0 +1,24 @@
+/*
+Problem Description
+You are given an array of N integers, A1, A2 ,..., AN and an integer B. Return the of count of distinct numbers in all windows of size B.
+
+Formally, return an array of size N-B+1 where i'th element in this array contains number of distinct elements in sequence Ai, Ai+1 ,..., Ai+B-1.
+
+NOTE: if B > N, return an empty array.
+
+Input
+A = [1, 2, 1, 3, 4, 3]
+B = 3
+Output
+[2, 3, 3, 2]
+*/
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> solve(vector<int>&A,int B){
+    
+}
+int main(){
+    vector<int>v={1, 2, 1, 3, 4, 3};
+    vector<int>result;
+    result=solve(v,3);
+}
