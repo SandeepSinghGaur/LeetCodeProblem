81362ff7768c5edf24edb9fc36e018488155faac
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2022-06-30 13:22:21 +0530

    Zig-Zag level order traversal

--- Scaler/Advanced/Day-65-Tree#02/ZigZag-Level-Order-Traversal-BT.cpp
+++ Scaler/Advanced/Day-65-Tree#02/ZigZag-Level-Order-Traversal-BT.cpp
@@ -20,3 +20,84 @@ Output
    [15, 7]
  ]
 */
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/* Definition for binary tree */
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+  vector<vector<int>>ans;
+void zigzag(TreeNode *root){
+     if(root==NULL) return ;
+     queue<TreeNode *>q;
+     stack<int>st;
+     q.push(root);
+     vector<int>v;
+     int k=0;
+     while(!q.empty()){
+         v.clear();
+         int sz=q.size();
+         for(int i=0;i<sz;i++){
+             TreeNode *temp=q.front(); 
+             q.pop();
+             v.push_back(temp->val);
+             if(temp->left!=NULL) q.push(temp->left);
+             if(temp->right!=NULL) q.push(temp->right);
+         }
+         if(k%2==0){
+            ans.push_back(v);
+         }else{
+             for(int i=0;i<v.size();i++){
+                 st.push(v[i]);
+             }
+             int p=0;
+             while(!st.empty()){
+               int ele=st.top();
+               st.pop();
+               v[p]=ele;
+               p++;
+             }
+              ans.push_back(v);
+         }
+         k++; 
+     }
+ }
+vector<vector<int> > zigzagLevelOrder(TreeNode* A) {
+    ans.clear();
+    zigzag(A);
+    return ans;
+}
+
+ int main(){
+    //create Node
+    TreeNode *n1=new TreeNode(3);
+    TreeNode *n2=new TreeNode(9);
+    TreeNode *n3=new TreeNode(20);
+    TreeNode *n4=new TreeNode(15);
+    TreeNode *n5=new TreeNode(7);
+  
+    //Link the Node
+    n1->right=n3;
+    n1->left=n2;
+    n3->left=n4;
+    n3->right=n5;
+    //call the helper function
+    vector<vector<int>>result;
+    result=zigzagLevelOrder(n1);
+    for(int i=0;i<result.size();i++){
+        for(int j=0;j<result[i].size();j++){
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+  
+
+ }
